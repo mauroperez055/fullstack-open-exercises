7621bad907477b2b4a39b9d7c4107809e0dd6c75
mauroperez055 <mauroperez055@gmail.com>
2025-06-30 21:35:32 -0300

    Avance de la aplicacion con los cambios propuesto en el ejercicio 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const exercises2 = 7;
   const part3 = 'State of a component';
   const exercises3 = 14;
-
+  
   return (
     <div>
       <Header course = {course} />
@@ -29,19 +29,30 @@ const Header = (props) => {
   )
 }
 
+const Part = ({ part, exercises }) => {
+  
+  return(
+    <div>
+      <p>{part} {exercises}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   console.log(props);
+
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part = {props.part1} exercises = {props.exercises1}  />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
     </div>
   )
 }
 
 const Total = (props) => {
   console.log(props);
+
   return (
     <div>
       <p>Number of exercises {props.total}</p>
