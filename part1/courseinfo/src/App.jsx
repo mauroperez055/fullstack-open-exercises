5e5e16712e946d3327953f5dba63920ae3b97fea
mauroperez055 <mauroperez055@gmail.com>
2025-07-02 19:57:05 -0300

    Actualizo el contenido del ejercicio 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,35 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+  }
   
   return (
     <div>
       <Header course = {course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
 
 const Header = (props) => {
   console.log(props);
-  return <p>{props.course}</p>
+  return <p>{props.course.name}</p>
+  
 }
 
 const Part = (props) => {
