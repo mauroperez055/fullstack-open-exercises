4024935879a9f729a8c0b098e97dfdd4eee5527e
mauroperez055 <mauroperez055@gmail.com>
2025-07-20 20:53:49 -0300

    Agrego los cambios del ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,14 +12,42 @@ const Statistic = ({ text, value }) => {
   return <div>{text} {value}</div>
 }
 
+const All = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad;
+
+  return (
+    <Statistic text='all' value={total} />
+  )
+}
+
+const Average = ({ points, good, neutral, bad }) => {
+  console.log('puntaje acumulado', points);
+  const total = good + neutral + bad;
+  const average = ( total === 0 ? 0 : (points/total));
+
+  return (
+    <div>average {(average)}</div>
+  )
+}
+
+const Positive = ({ good, neutral, bad}) => {
+  const positive = good === 0 ? 0 : (good * 100 / (good + neutral + bad));
+
+  return (
+    <p>positive {positive}%</p>
+  )
+}
+
 const App = () => {
   
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [points, setPoints] = useState(0);
 
   const handleClickGood = () => {
     setGood(good + 1);
+    setPoints(points + 1);
   }
 
   const handleClickNeutral = () => {
@@ -28,6 +56,7 @@ const App = () => {
 
   const handleClickBad = () => {
     setBad(bad + 1);
+    setPoints(points - 1);
   }
   
   return(
@@ -40,6 +69,9 @@ const App = () => {
       <Statistic text='good' value={good}/> 
       <Statistic text='neutral' value={neutral}/> 
       <Statistic text='bad' value={bad}/> 
+      <All good={good} neutral={neutral} bad={bad} />
+      <Average points={points} good={good} neutral={neutral} bad={bad} />
+      <Positive good={good} neutral={neutral} bad={bad} />
     </div>
   )
 
