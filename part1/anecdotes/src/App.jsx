4938a898b75539c9d1df3a7fffe9186001a7c190
mauroperez055 <mauroperez055@gmail.com>
2025-07-27 19:11:06 -0300

    Agrego el ejercicio 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,58 +1,72 @@
 import { useState } from 'react'
 
-const Anecdotes = ({anecdotes, selected, votes}) => {
+const Anecdotes = ({ anecdotes, selected, votes }) =>{
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
-      <br></br>
-      has {votes[selected]} votes 
+      <br />
+      has {votes[selected]} votes
     </div>
   )
 }
 
-const Button = ({ handleClick, text }) => {
+const MostAnecdotes = ({ anecdotes, maxIndex, votes }) => {
   return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[maxIndex]}
+      <br />
+      has {votes[maxIndex]} votes
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return(
     <button onClick={handleClick}>{text}</button>
   )
 }
 
-
-function App() {
+const App = () => {
   const anecdotes = [
-    'If it hurst, do it more often.',
-    'Adding manpower to a late software project majes is later!',
-    'The first 90 percent of the code account for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good progammers write code that humans can understand.',
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debbugin is twice has hard has writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood test when diagnosing patients.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
 
   const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Uint8Array(8));
-
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
+  
   const handleAnecdote = () => {
-    let anecdoteRandom;
-    do{
-      anecdoteRandom = Math.floor(Math.random() * anecdotes.length);
-    } while (anecdoteRandom === selected);
-    setSelected(anecdoteRandom);
-    console.log(anecdoteRandom);
+    let randomAnecdote;
+    do {
+      randomAnecdote = Math.floor(Math.random() * anecdotes.length);
+    } while ( randomAnecdote === selected);
+    console.log('randomAnecdote:', randomAnecdote);
+    setSelected(randomAnecdote);
   }
 
-  const handleVotes = () => {
+  const handleVote = () => {
     const copy = [...votes];
-    copy[selected] +=1;
-    console.log('copy actualizado', copy);
+    copy[selected] += 1;
     setVotes(copy);
+    console.log('copy updated: ',copy);
   }
 
+  const maxIndex = votes.indexOf(Math.max(...votes));
+  
   return (
     <div>
       <Anecdotes anecdotes={anecdotes} selected={selected} votes={votes}/>
-      <Button handleClick={handleVotes} text='vote'/>
-      <Button handleClick={handleAnecdote} text='next anecdote'/>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleAnecdote} text="next anecdote" />
+      <MostAnecdotes anecdotes={anecdotes} maxIndex={maxIndex} votes={votes}/>
     </div>
   )
 }
