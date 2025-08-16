2f4f4831fa184c40be7a63de06b2f59b9a597c4e
mauroperez055 <mauroperez055@gmail.com>
2025-08-15 21:12:28 -0300

    Agrego ejercicio 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
-import axios from 'axios';
+import personService from './services/persons';
 
 const App = () => {
   const [persons, setPersons] = useState([]); 
@@ -11,10 +11,10 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3002/persons')
-      .then(response => {
-        setPersons(response.data);
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
       })
   }, []);
   console.log('render', persons.length, ' persons');
@@ -31,10 +31,10 @@ const App = () => {
       setNewName('');
       setNewNumber('');
     } else {
-      axios
-        .post('http://localhost:3002/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data));
+        personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson));
           setNewName('');
           setNewNumber('');
         })
